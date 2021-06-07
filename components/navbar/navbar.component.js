4b19d6959ea9516e647bf50a22f655ad742c0d7d
Wezley <wezley.singleton@gmail.com>
2021-06-06 21:04:06 -0400

    navbar in place

--- components/navbar/navbar.component.js
+++ components/navbar/navbar.component.js
@@ -1,11 +1,41 @@
-NavbarComponent.prototype = new ViewComponent('login');
-let navbarComponent = new NavbarComponent();
-navbarComponent.render();
+const NAVBAR_ELEMENT = document.getElementById('navbar');
 
 function NavbarComponent() {
+
+    let templateHolder = '';
+    let frag = `components/navbar/navbar.component`;
+
+    function injectTemplate (cb) {
+        if (templateHolder) {
+            NAVBAR_ELEMENT.innerHTML = templateHolder;
+        } else {
+            fetch(`${frag}.html`)
+                .then(resp => resp.text())
+                .then(body => {
+                    templateHolder = body
+                    NAVBAR_ELEMENT.innerHTML = templateHolder;
+                    cb();
+                })
+                .catch(err => console.error(err))
+        }
+
+    }
+
+    function injectStylesheet() {
+        let stylesheet = document.getElementById('dynamic-css');
+        if (stylesheet) stylesheet.remove();
+        stylesheet = document.createElement('link');
+        stylesheet.id = 'dynamic-css'
+        stylesheet.rel = 'stylesheet';
+        stylesheet.href = `${frag}.css`;
+        document.head.appendChild(stylesheet);
+    }
     
     this.render = function() {
-        document.getElementById('navbar').innerHTML = '<h1>NavbarComponent works!</h1>';
+        injectStylesheet();
+        injectTemplate(() => {
+            console.log('navbar rendered');
+        });
     }
 
 }
